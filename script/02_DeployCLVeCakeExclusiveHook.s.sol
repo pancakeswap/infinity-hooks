2fb1e208f7ffea293ffb2da3fc146a9e43bf9680
chef-burger <137024020+chefburger@users.noreply.github.com>
2025-02-10 17:08:29 +0800

    deploy: redeploy (#25)

--- script/02_DeployCLVeCakeExclusiveHook.s.sol
+++ script/02_DeployCLVeCakeExclusiveHook.s.sol
@@ -21,7 +21,7 @@ import {Create3Factory} from "pancake-create3-factory/src/Create3Factory.sol";
  */
 contract DeployCLVeCakeExclusiveHookScript is BaseScript {
     function getDeploymentSalt() public pure override returns (bytes32) {
-        return keccak256("INFINITY-HOOKS/CLVeCakeExclusiveHook/0.90");
+        return keccak256("INFINITY-HOOKS/CLVeCakeExclusiveHook/0.97");
     }
 
     function run() public {
